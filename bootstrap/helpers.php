b729c6a347c6aa13b9a28ba3d0d961b223d14ce9
vagrant <vagrant@homestead>
2018-06-22 13:32:27 +0000

    完成公共页面布局

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -2,3 +2,14 @@
 /**
  *  辅助函数库
  */
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * 将请求的路由名称转换为css类名称
+ * @return mixed
+ */
+function route_class()
+{
+    return str_replace('.', '-', Route::currentRouteName());
+}
