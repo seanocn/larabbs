58f7f9bfadb0587c20d473d1162c082ffcb69b8e
vagrant <vagrant@homestead>
2018-06-23 15:58:50 +0000

    Topic index page

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,4 +5,16 @@ namespace App\Models;
 class Topic extends Model
 {
     protected $fillable = ['title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+
+    //话题分类模型
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //话题作者模型
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
