1bc0ffe6abed5321b9d31cce676666555f670879
vagrant <vagrant@homestead>
2018-06-24 03:46:54 +0000

    分类主题列表完成

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes(); //用户认证路由
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
